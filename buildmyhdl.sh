4b7941e8310086f4f9f0be7fbdb119b968f57383
mtvjr <mtv27@txstate.edu>
2018-04-02 11:37:15 -0500

    Fixed buildmyhdl again

--- buildmyhdl.sh
+++ buildmyhdl.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 pushd $(dirname "${0}") > /dev/null
-ROOTD=$(pwd -L)
-echo "$ROOTD"
-pushd "$ROOTD/lib/myhdl" > /dev/null
+BASED=$(pwd -L)
+echo "Using $BASED as base directory"
+pushd "$BASED/lib/myhdl" > /dev/null
 
 echo Downloading myhdl library
+git submodule init
 git submodule update
 if [ ! -d "cosimulation" ]; then
    echo Error: Unable to download myhdl
    exit
 fi
-cd myhdl/cosimulation/icarus
+cd "$BASED/lib/myhdl/cosimulation/icarus"
 
 echo Building myhdl.vpi
 make
@@ -20,12 +21,12 @@ if [ ! -f "myhdl.vpi" ]; then
 fi
 
 echo Copying myhdl.bpi to bin
-cp myhdl.vpi "$ROOTD/bin/myhdl.vpi"
-if [ ! -f "$ROOTD/bin/myhdl.vpi" ]; then
+cp myhdl.vpi "$BASED/bin/myhdl.vpi"
+if [ ! -f "$BASED/bin/myhdl.vpi" ]; then
    echo Error: Unable to copy myhdl.vpi to bin
    exit
 fi
-echo myhdl.bpi created at "$ROOTD/bin/myhdl.vpi"
+echo myhdl.bpi created at "$BASED/bin/myhdl.vpi"
 
 # Return to the original directory
 popd > /dev/null

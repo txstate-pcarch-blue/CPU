c81817e6ee28bbcd6ead0b9cd735e1362c7c2581
KGMaxey <kgm46@txstate.edu>
2018-04-06 10:15:16 -0500

    Fixed @always block on ALU_Control

--- python/ALU_Control.py
+++ python/ALU_Control.py
@@ -3,9 +3,9 @@ from myhdl import *
 @block
 def ALU_Control(clk, op, out): #Clk and OpCode input, int output
 
-    @always(clk.posedge):
+    @always(clk.posedge)
     def execute():
         #Insert op code interpretation
         return
-        
+
     return execute

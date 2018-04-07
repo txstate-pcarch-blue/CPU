c7e1894ed798d4ae08ccfef3749038bc9eecc4d6
Cody <Cody.Sears@g.austincc.edu>
2018-04-07 01:01:11 -0500

    Completed Register and Register TB. Output tested in GTKWave. (#5)
    * Completed Register and Register TB. Output tested in GTKWave.
    
    * Delete Register_ah.vcd
    
    Will not include .vcd files in pull requests.
    
    * Added comma inside comment of Register.v line 1.
    
    Thanks for noting the missing comma.  I noticed this was missing in the starter code and added the comma for testing and removed it afterwards, but I think including inside the comment tag and moving the comment tag up makes it easier for testing.  That was part of why I thought I might include the .vcd file to save the trouble of others missing the comma, etc.  This is better.
    
    * Comma moved, num_iter updated
    
    Moved comma inside comment of Register_tb.v line 61.  Updated ++ num_iter; to num_iter = num_iter + 1; of Register_tb.v line 129.

--- verilog/Register.v
+++ verilog/Register.v
@@ -1,18 +1,19 @@
-module RegisterFile(BusA, BusB, BusW, RA, RB, RW, RegWr, Clk, Rst
-/*
-regOut0,regOut1,regOut2,regOut3,regOut4,regOut5,regOut6,regOut7,regOut8,
-regOut9,regOut10,regOut11,regOut12,regOut13,regOut14,regOut15
+module RegisterFile(BusA, BusB, BusW, RA, RB, RW, RegWr, Clk, Rst/*,
+regOut0,regOut1,regOut2,regOut3,regOut4,regOut5,regOut6,regOut7,
+regOut8,regOut9,regOut10,regOut11,regOut12,regOut13,regOut14,regOut15,
+regOut16,regOut17,regOut18,regOut19,regOut20,regOut21,regOut22,regOut23,
+regOut24,regOut25,regOut26,regOut27,regOut28,regOut29,regOut30,regOut31
 */
 );
+
+//Inputs: Read Register 1, Read Register 2, Write Register (5bits), Write Data (32bits), Write Control
 	output reg [31:0] BusA, BusB;  //data to read
+	input [4:0] RA, RB, RW;  //read addr A, read addr B, write addr
 	input [31:0] BusW;  //data to write
-	input [3:0] RA, RB, RW;  //read addr A, read addr B, write addr
 	input RegWr;  //write ctrl
 	input Clk, Rst;
 	
-	
-	
-	reg [31:0] registers [15:0];
+	reg [31:0] registers [31:0];
 	
 	/* 
 	* FOR DEBUG registers to view on GTKWave
@@ -35,6 +36,22 @@ regOut9,regOut10,regOut11,regOut12,regOut13,regOut14,regOut15
 		output [31:0] regOut13;
 		output [31:0] regOut14;
 		output [31:0] regOut15;
+		output [31:0] regOut16;
+		output [31:0] regOut17;
+		output [31:0] regOut18;
+		output [31:0] regOut19;
+		output [31:0] regOut20;
+		output [31:0] regOut21;
+		output [31:0] regOut22;
+		output [31:0] regOut23;
+		output [31:0] regOut24;
+		output [31:0] regOut25;
+		output [31:0] regOut26;
+		output [31:0] regOut27;
+		output [31:0] regOut28;
+		output [31:0] regOut29;
+		output [31:0] regOut30;
+		output [31:0] regOut31;
 	
 		assign regOut0 = registers[0];
 		assign regOut1 = registers[1];
@@ -52,13 +69,28 @@ regOut9,regOut10,regOut11,regOut12,regOut13,regOut14,regOut15
 		assign regOut13 = registers[13];
 		assign regOut14 = registers[14];
 		assign regOut15 = registers[15];
+		assign regOut16 = registers[16];
+		assign regOut17 = registers[17];
+		assign regOut18 = registers[18];
+		assign regOut19 = registers[19];
+		assign regOut20 = registers[20];
+		assign regOut21 = registers[21];
+		assign regOut22 = registers[22];
+		assign regOut23 = registers[23];
+		assign regOut24 = registers[24];
+		assign regOut25 = registers[25];
+		assign regOut26 = registers[26];
+		assign regOut27 = registers[27];
+		assign regOut28 = registers[28];
+		assign regOut29 = registers[29];
+		assign regOut30 = registers[30];
+		assign regOut31 = registers[31];
 	*/
-	
 	
 	integer i, j;
 	
 	initial begin 
-		for(j = 0; j < 16; j = j + 1) begin
+		for(j = 0; j < 32; j = j + 1) begin
 				registers[j] <= 0;
 		end
 	end
@@ -68,7 +100,7 @@ regOut9,regOut10,regOut11,regOut12,regOut13,regOut14,regOut15
 			if (RegWr)
 				registers[RW] <= BusW;
 			else if (Rst) begin
-				for(i = 0; i < 16; i = i + 1) 
+				for(i = 0; i < 32; i = i + 1) 
 					registers[i] <= 0;
 			end
 		end
--- verilog/Register_tb.v
+++ verilog/Register_tb.v
@@ -2,16 +2,16 @@
 
 module tb();
 
-	parameter tck = 10; ///< clock tick
+	parameter tck = 10; // clock tick
   
 	reg clk, rst;
 	reg [31:0]W;
-	reg [3:0] RA, RB, RW;
+	reg [4:0] RA, RB, RW;
 	reg wrn;
   
 	wire [31:0] A, B;
 	
-	integer seed = 1 ; 
+	integer seed = 1;
 	
 	/*
 		wire [31:0] regOut0;
@@ -30,6 +30,22 @@ module tb();
 		wire [31:0] regOut13;
 		wire [31:0] regOut14;
 		wire [31:0] regOut15;
+		wire [31:0] regOut16;
+		wire [31:0] regOut17;
+		wire [31:0] regOut18;
+		wire [31:0] regOut19;
+		wire [31:0] regOut20;
+		wire [31:0] regOut21;
+		wire [31:0] regOut22;
+		wire [31:0] regOut23;
+		wire [31:0] regOut24;
+		wire [31:0] regOut25;
+		wire [31:0] regOut26;
+		wire [31:0] regOut27;
+		wire [31:0] regOut28;
+		wire [31:0] regOut29;
+		wire [31:0] regOut30;
+		wire [31:0] regOut31;
 	*/
 	
 	RegisterFile dut
@@ -42,37 +58,50 @@ module tb();
 		.RW(RW),
 		.RegWr(wrn),
 		.Clk(clk),
-		.Rst(rst)
-		
-		/*
-			.regOut0(regOut0),
-			.regOut1(regOut1),
-			.regOut2(regOut2),
-			.regOut3(regOut3),
-			.regOut4(regOut4),
-			.regOut5(regOut5),
-			.regOut6(regOut6),
-			.regOut7(regOut7),
-			.regOut8(regOut8),
-			.regOut9(regOut9),
-			.regOut10(regOut10),
-			.regOut11(regOut11),
-			.regOut12(regOut12),
-			.regOut13(regOut13),
-			.regOut14(regOut14),
-			.regOut15(regOut15)
-		*/
+		.Rst(rst)/*,
+		.regOut0(regOut0),
+		.regOut1(regOut1),
+		.regOut2(regOut2),
+		.regOut3(regOut3),
+		.regOut4(regOut4),
+		.regOut5(regOut5),
+		.regOut6(regOut6),
+		.regOut7(regOut7),
+		.regOut8(regOut8),
+		.regOut9(regOut9),
+		.regOut10(regOut10),
+		.regOut11(regOut11),
+		.regOut12(regOut12),
+		.regOut13(regOut13),
+		.regOut14(regOut14),
+		.regOut15(regOut15),
+		.regOut16(regOut16),
+		.regOut17(regOut17),
+		.regOut18(regOut18),
+		.regOut19(regOut19),
+		.regOut20(regOut20),
+		.regOut21(regOut21),
+		.regOut22(regOut22),
+		.regOut23(regOut23),
+		.regOut24(regOut24),
+		.regOut25(regOut25),
+		.regOut26(regOut26),
+		.regOut27(regOut27),
+		.regOut28(regOut28),
+		.regOut29(regOut29),
+		.regOut30(regOut30),
+		.regOut31(regOut31)
+	*/
 	);
 	  
 	integer num_iter = 0 ;
 	always #(tck/2) clk <= ~clk; // clocking device
 	  
 	initial begin
-		$dumpfile("register_ah.vcd"); 
+		$dumpfile("Register_ah.vcd"); 
 		$dumpvars (-1, dut);
 	end
-	  
-	  
+
 	initial begin
 		clk = 0; rst = 0; wrn = 0;
 		RA  = 4'd0;
@@ -86,10 +115,18 @@ module tb();
 		RB  = $random(seed);
 		RW  = $random(seed);
 		W   = $random(seed);
-		rst = $random(seed);
 		wrn = $random(seed);
+
+		// limit random reset to every 10 cycles
+		// to make results more readable
+		if (num_iter % 10 == 1) begin
+			rst = $random(seed);	
+		end
+		else begin
+			rst = 0;
+		end
 		
-		++ num_iter;
+		num_iter = num_iter + 1;
 	end
 	
 	always @(negedge clk) begin

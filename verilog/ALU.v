08a770343991d2599647dc9c47b9b7dee4a52a1c
Daniel Le <ledanieln@gmail.com>
2018-04-09 11:01:48 -0500

    Verilog ALU Update (#9)
    * Added overflow flag to ALU.v; Register results are only producing every other clock cycle.
    
    * Got the ALU to produce an output register every positive clock cycle.
    
    * Remove test files
    
    * Added support for branch conditional, added branch testing in test bench, and simplified test bench
    
    * Still need to add branch tester
    
    * Fixed ALU output to output every positive edge. Make sure inputs in test bench are unblocking assignments.
    
    * Changed ++num_iter to num_iter = num_iter + 1

--- verilog/ALU.v
+++ verilog/ALU.v
@@ -1,26 +1,42 @@
-module alu  (input [15:0] A,B,
-             input [2:0] CTRL,
+module alu  (input [31:0] A,B,
+             input [1:0] CTRL,
              input clk, reset,
-             output reg [15:0] R,
-			 output zero);
+             output reg [31:0] R,
+			 output zero,
+			 output reg ovf, branch);
 		
-		assign zero = (R==0); //Zero is true if output y is 0
+		assign zero = (R==0); //Zero is true if output r is 0
+		
         /* Decode the instruction */
+
         always @(posedge clk) begin
 			if(reset == 1) begin
 				R <= 16'b0;
 			end
-			else if(reset == 0) begin
+
+			else begin
+				ovf <= 0;
+				branch <= 0;
 				case (CTRL)
-					3'h1 /* ADD */:   R <= A + B;
-					3'h2 /* XOR */:   R <= A ^ B;
-					3'h3 /* AND */:   R <= A & B;
-					3'h4 /* OR */:    R <= A | B;
-					3'h5 /* NOTA */:  R <= ~A;
-					3'h6 /* NAND */:  R <= ~(A & B);
-					3'h7 /* NOR */:   R <= ~(A | B);
+
+					2'b00 /* ADD */: {ovf, R} <= A + B;
+					2'b01 /* SUB */: {ovf, R} <= A - B;
+					2'b10 /* XOR */: R <= A ^ B;
+					2'b11 /* Branch Conditional */: begin
+						if (A == B) branch <= 1;
+						else R <= 0;
+						end
+
+
+					//3'h2 /* XOR */:   R <= A ^ B;
+					//3'h3 /* AND */:   R <= A & B;
+					//3'h4 /* OR */:    R <= A | B;
+					//3'h5 /* NOTA */:  R <= ~A;
+					//3'h6 /* NAND */:  R <= ~(A & B);
+					//3'h7 /* NOR */:   R <= ~(A | B);
+
 					default: R <= 0;
 				endcase
 			end
 		end
-endmodule //alu
+endmodule 
--- verilog/ALU_tb.v
+++ verilog/ALU_tb.v
@@ -4,11 +4,13 @@ module alu_tb();
 
 	parameter tck = 10; ///< clock tick
 
-	reg [15:0] a, b ;
-	reg clk , reset ;
-	reg [2:0] ctrl  ;
-	wire [15:0] r ;
+	reg [31:0] a, b ;
+	reg clk, reset;
+	reg [1:0] ctrl ;
+	wire [31:0] r ;
 	wire zero ;
+	wire ovf ;
+	wire branch ;
 
 
 	alu dut(	.A(a), 
@@ -17,7 +19,9 @@ module alu_tb();
 				.CTRL(ctrl), 
 				.reset(reset), 
 				.R(r), 
-				.zero(zero) 
+				.zero(zero),
+				.ovf(ovf), 
+				.branch(branch)
 			);
 			
 	integer num_iter = 0 ;
@@ -30,33 +34,22 @@ module alu_tb();
 		$monitor("%b", r);
 	end
 							
-	//Initialise registers 
+	//Initialise registers
+
 	initial begin
-		clk = 0; reset = 0; 
-		#(tck*2);
-		reset = 1;
-		#(tck*2);
-		reset = 0;
-		#(tck*2);
+		clk = 0 ; 
+		reset = 0 ;
 		a = 0 ;
 		b = 0 ;
+		ctrl = 0 ;
 	end
 	
 	always @(posedge clk) begin
-		if (reset) begin
-			reset = 0;
-		end
-		else begin
-			a = $random(seed) ;
-			b = $random(seed) ;
-			ctrl = $random(seed) ;
-			++ num_iter ;
-		end
-	end
-
-	always @(negedge clk) begin
-		if (num_iter > 99999)
-			$finish;
+		a <= $random(seed) ;
+		b <= $random(seed) ;
+		ctrl <= $random(seed) ;
+		num_iter = num_iter+1 ;
+		if (num_iter > 999) $finish;
 	end
 
 endmodule

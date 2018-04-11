c4b86292ca5a0443726fe2984a587e0329dfaa9c
mtvjr <mtv27@txstate.edu>
2018-04-10 19:29:14 -0500

    Python program counter added

--- python/PC.py
+++ python/PC.py
@@ -1,6 +1,16 @@
-from myhdl import *
+from myhdl import block, always
 
+# Program Counter
+# Adds 4 to the current instruction address each clock cycle
+# Changes the current instruction address to addr_in if given a branch signal
 @block
-def PC():
+def program_counter(clock, addr_out, addr_in, branch_signal):
+    @always(clock.posedge)
+    def program_counter():
+      if (branch_signal):
+        addr_out.next = addr_in
+      else:
+        addr_out.next = addr_out + 4
+      print("PC: ", addr_out)
 
-    return
+    return program_counter

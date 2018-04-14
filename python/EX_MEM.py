46a404b06e85d9d9f557f2f7df73d2e9e078656a
KGMaxey <kgm46@txstate.edu>
2018-04-14 14:17:18 -0500

    Added more registers and fixed single bit issue

--- python/EX_MEM.py
+++ python/EX_MEM.py
@@ -2,8 +2,8 @@ from myhdl import *
 
 @block
 def ex_mem(clk, ALU_in, ALU_out, MemData_in, MemData_out, reg_in,
-            reg_out, zero_in, zero_out, PC_in, PC_out, MemtoReg_in,
-            MemtoReg_out, MemWrite_in, MemWrite_out, WB_in, WB_out):
+            reg_out, zero_in, zero_out, PC_in, PC_out, MemToReg_in,
+            MemToReg_out, MemWrite_in, MemWrite_out, WB_in, WB_out):
 
     class latches:
         ALU = intbv(0, 0, 2**32)
@@ -11,7 +11,7 @@ def ex_mem(clk, ALU_in, ALU_out, MemData_in, MemData_out, reg_in,
         reg = intbv(0, 0, 2**5)
         zero = intbv(0, 0, 2**1)
         PC = intbv(0, 0, 2**32)
-        MemtoReg = intbv(0, 0, 2**1)
+        MemToReg = intbv(0, 0, 2**1)
         MemWrite = intbv(0, 0, 2**1)
         WB = intbv(0, 0, 2**1)
 
@@ -20,7 +20,7 @@ def ex_mem(clk, ALU_in, ALU_out, MemData_in, MemData_out, reg_in,
     @always(clk.posedge)
     def input():
         latches.ALU = ALU_in
-        latches.MemData =
+        latches.MemData = MemData_in
         latches.reg = reg_in
         latches.zero = zero_in
         latches.PC = PC_in
@@ -30,7 +30,7 @@ def ex_mem(clk, ALU_in, ALU_out, MemData_in, MemData_out, reg_in,
 
     @always(clk.negedge)
     def output():
-        Alu_out.next = latches.ALU
+        ALU_out.next = latches.ALU
         MemData_out.next = latches.MemData
         reg_in.next = latches.reg
         zero_out.next = latches.zero
--- python/EX_MEM_tb.py
+++ python/EX_MEM_tb.py
@@ -6,35 +6,43 @@ from Random_Signal import random_signal
 if (__name__ == "__main__"):
     MAX_CYCLES = 1000
 
-    clk_in = Signal(0)
+    clk = Signal(intbv(0, 0, 2**1))
+
     ALU_in = Signal(intbv(0, 0, 2**32))
     MemData_in = Signal(intbv(0, 0, 2**32))
     reg_in = Signal(intbv(0, 0, 2**5))
     zero_in = Signal(intbv(0, 0, 2**1))
     PC_in = Signal(intbv(0, 0, 2**32))
-    MemtoReg_in = Signal(intbv(0, 0, 2**1))
+    MemToReg_in = Signal(intbv(0, 0, 2**1))
     MemWrite_in = Signal(intbv(0, 0, 2**1))
     WB_in = Signal(intbv(0, 0, 2**1))
 
-    clk_out = Signal(0)
     ALU_out = Signal(intbv(0, 0, 2**32))
     MemData_out = Signal(intbv(0, 0, 2**32))
     reg_out = Signal(intbv(0, 0, 2**5))
     zero_out = Signal(intbv(0, 0, 2**1))
     PC_out = Signal(intbv(0, 0, 2**32))
-    MemtoReg_out = Signal(intbv(0, 0, 2**1))
+    MemToReg_out = Signal(intbv(0, 0, 2**1))
     MemWrite_out = Signal(intbv(0, 0, 2**1))
     WB_out = Signal(intbv(0, 0, 2**1))
+
 
     clock_driver = clock_generator(clk)
 
     register_driver = traceSignals(ex_mem(clk, ALU_in, ALU_out, MemData_in, MemData_out, reg_in,
-                                    reg_out, zero_in, zero_out, PC_in, PC_out, MemtoReg_in,
-                                    MemtoReg_out, MemWrite_in, MemWrite_out, WB_in, WB_out))
+                                    reg_out, zero_in, zero_out, PC_in, PC_out, MemToReg_in,
+                                    MemToReg_out, MemWrite_in, MemWrite_out, WB_in, WB_out))
     ALU_driver = random_signal(ALU_in, clk)
     MemData_driver = random_signal(MemData_in, clk)
     reg_driver = random_signal(reg_in, clk)
+    zero_driver = random_signal(zero_in, clk)
+    PC_driver = random_signal(PC_in, clk)
+    MemToReg_driver = random_signal(MemToReg_in, clk)
+    MemWrite_driver = random_signal(MemWrite_in, clk)
+    WB_driver = random_signal(WB_in, clk)
 
 
-    sim = Simulation(clock_driver, register_driver, inst_driver, PC_driver)
+    sim = Simulation(clock_driver, register_driver, ALU_driver, MemData_driver,
+                        reg_driver, zero_driver, PC_driver, MemToReg_driver, MemWrite_driver,
+                        WB_driver)
     sim.run(MAX_CYCLES)

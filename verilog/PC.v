5766fe8e0da5524aff82f145f0e3fe360a0057e7
Daniel Le <LeDaniel@Daniel-MacBook.local>
2018-04-15 20:46:20 -0500

    Added comments to PC.v file

--- verilog/PC.v
+++ verilog/PC.v
@@ -1,3 +1,8 @@
+// 32-bit PC
+// data input width: 1 32-bit value (branch addr, PC+4, or jump addr)
+// data output width: 1 32-bit addr
+// control: 1-bit: branch or not branch
+
 module pc  (input [31:0] addrIn,
              input clk, reset, branch,
              output reg [31:0] addrOut);

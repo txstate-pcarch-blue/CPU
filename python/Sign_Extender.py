b092b902344a02fbefacc1089110c2c316dbbde4
MMaluschka13 <MMaluschka13@gmail.com>
2018-04-18 13:01:12 -0500

    Started sign extender.

--- python/Sign_Extender.py
+++ python/Sign_Extender.py
@@ -4,12 +4,12 @@ from myhdl import *
 def Sign_Extender(clk, instruction, output):
     #Instruction should be a 16-bit intbv
     #output is accurate 32-bit intbv.
-    output = intbv(max = 32)
+    output.Next = intbv(max = 32)
     
     @alway(clk.posedge)
     def execute():
         #Insert 16 to 32-bit conversion loginc
-
-        return
+        output[31:16].Next = instruciton[15] #make the first new bits match the first bit
+        return output.Next
 
     return execute

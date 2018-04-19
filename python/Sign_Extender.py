a6518118bc594d81afb87cf948ee9d6cdc76afce
MMaluschka13 <MMaluschka13@gmail.com>
2018-04-19 10:36:47 -0500

    Added out variable

--- python/Sign_Extender.py
+++ python/Sign_Extender.py
@@ -1,10 +1,10 @@
 from myhdl import *
 
 @block
-def Sign_Extender(clk, instruction, output):
+def Sign_Extender(clk, instruction, output, out):
     #Instruction should be a 16-bit intbv
     #output is accurate 32-bit intbv.
-    output.Next = intbv(min = 0, max = 2**32)
+    output = intbv(min = 0, max = 2**32)
     
     @alway(clk.posedge)
     def execute():

dc6907372f4aeb622693259ccca3c44a85dacfe1
MMaluschka13 <MMaluschka13@gmail.com>
2018-04-20 12:54:34 -0500

    Fixed unit tests for Data_Memory and Instruction Memory as well as Sign Extender.

--- python/Data_Memory.py
+++ python/Data_Memory.py
@@ -1,10 +1,9 @@
 from myhdl import *
 
 @block
-def Data_Memory(clk, address, read, readData, writeData):
+def Data_Memory(clk, address, write, readData, writeData):
     #modeled inputs and outputs to match model below
 
-    mem = [intbv(max = 2**32)] * 256 #create a mem array with 256 32-bit intbv elements
     
     #Use this link's datapath as an example (http://d2vlcm61l7u1fs.cloudfront.net/media%2F63b%2F63b8364b-1ba0-467c-87f2-60b74041b998%2FphpArhSpB.png)
     #Be sure to note that the control line will determine multiplexor
@@ -13,12 +12,11 @@ def Data_Memory(clk, address, read, readData, writeData):
     @always(clk.posedge)
     def execute():
         #Insert Memory Read or Write
-        if read == 0: 
-            readData.next = mem[address[9:2]] #return memory to be loaded into a register
-            return
-        elif read == 1:
-            mem[address[9:2]] = writeData #write register data into memory
-        return
+        #readData.next = execute.mem[address[9:2]] #return memory to be loaded into a register
+        if write:
+            execute.mem[address[9:2]] = writeData #write register data into memory
+        readData.next = execute.mem[address[9:2]]
 
+    execute.mem = [intbv(max = 2**32)] * 256 #create a mem array with 256 32-bit intbv elements
     return execute
 
--- python/Instruction_Memory.py
+++ python/Instruction_Memory.py
@@ -3,11 +3,10 @@ from myhdl import *
 @block
 def Instruction_Memory(clk, address, out): #Addr and clk input, out is output
     
-    mem = [intbv(max = 2**32)] * 256 #create 256 32-bit memory array
 
     @always(clk.posedge)
     def execute():
-        out.next = mem[address[9:2]] #return the address of the instruciton
-        return
+        out.next = execute.mem[address[9:2]] #return the address of the instruciton
 
+    execute.mem = [intbv(max = 2**32)] * 256
     return execute

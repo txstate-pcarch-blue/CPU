3ba871aab27c4cffd5ab990963ed8779ac1c574d
KGMaxey <kgm46@txstate.edu>
2018-04-21 14:08:30 -0500

    Added Control Unit Barebones, will update after work

--- python/Control.py
+++ python/Control.py
@@ -1,6 +1,25 @@
 from myhdl import *
 
 @block
-def Control():
+def Control(opcode, ALUSrc, RegDst, MemWrite, MemRead, Beq, Jump, MemToReg, RegWrite, ALUOp):
 
-    return
+    @always(*)
+    def control():
+        if(opcode==o):
+
+        elif(opcode==35):
+
+        elif(opcode==43):
+
+        elif(opcode==4):
+
+        elif(opcode==2):
+
+        elif(opcode==3):
+
+        elif(opcode==8):
+
+        elif(opcode==9):
+
+
+    return control

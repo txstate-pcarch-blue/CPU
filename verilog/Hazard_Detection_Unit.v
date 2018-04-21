dc4e4e4545610631c77a5b36c5eafa7ecfeef613
Boris <62696e617279@users.noreply.github.com>
2018-04-20 19:00:20 -0500

    Verilog branch (#33)
    * Boris: adding Control and Control_tb verilog files. Please note the funct and alu op codes do not match up to the spreadsheet and I will need to clean it up in the next update. Also added changes to ALU_Control and comment plus OR operation in ALU.v
    
    * Boris: changes as per feedback on pull request - removed vvp and ordered ALU.
    
    * deleted vvp
    
    * Boris: added ID_EX module and testbench
    
    * Boris: added ID_EX module and testbench
    
    * Boris: adding working EX_MEM and testbench. Also adding hazard and control unit code but please note that, for now, it is only intended for discussion and planning.
    
    * Boris: updated Hazard and Forwarding Units with code.

--- verilog/Hazard_Detection_Unit.v
+++ verilog/Hazard_Detection_Unit.v
@@ -1,82 +1,18 @@
+// We detect lw hazard b/w current instruction in ID stage and previous instruction in EX stage
+// Hazard occurs if prev instr. is lw
 module hazard_unit(
-	// Inputs
-	RsD, RtD, Branch, RsE, RtE, WriteRegE, MemtoRegE, RegWriteE, WriteRegM, 
-	MemtoRegM, RegWriteM, WriteRegW, RegWriteW, syscallD,
+    // Inputs
+    ID_EX_MemRead, ID_EX_RegRt, IF_ID_RegRs, IF_ID_RegRt
 
-	// Outputs
-	StallF, StallD, FlushE, ForwardAE, ForwardBE
+    // Outputs: two control signals, which determine if pipeline stalls or continues
+    // Mux select which forces control signals for current EX and future MEM_WB stage to 0
+    // in case of stall
+    PCWrite, IF_ID_Write, Mux_Select_Stall
 );
 
-	//1st source reg from Decode 
-	input [4:0] RsD;
-	//2nd source reg from Decode
-	input [4:0] RtD;
-	// Whether a branch instruction is executing, as determined by the EX stage.
-	input BranchEX;
-	//1st source reg from Execute
-	input [4:0] RsE;
-	//2nd source reg from Execute
-	input [4:0] RtE;
-	// The register that will be written to, as determined by the Execute stage.
-	input [4:0] WriteRegE;
-	// Whether data is being written from M->E, as determined by the Execute stage.
-	input MemtoRegE;
-	// Whether a register is written to, as determined by the Execute stage.
-	input RegWriteE;
-	// The register that will be written to, as determined by the Mem stage.
-	input [4:0] WriteRegM;
-	// Whether data is being written from M->E, as determined by the Mem stage.
-	input MemtoRegM;
-	// Whether a register is written to, as determined by the Mem stage.
-	input RegWriteM;
-	// The register that will be written to, as determined by the Writeback stage.
-	input [4:0] WriteRegW;
-	// Whether a register is written to, as determined by the Writeback stage.
-	input RegWriteW;
-	// Whether the decode stage is trying to do a syscall.
-	input syscallD;
-
-
-	// This flag is high when the Fetch stage needs to stall.
-	output StallF;
-	// This flag is high when the Decode stage needs to stall.
-	output StallD;
-	// True when the Execute stage needs to be flushed.
-	output wire FlushE;
-	// 1 for MEM/EX forwarding; 2 for EX/EX forwarding of Rs.
-	output wire [1:0] ForwardAE;
-	// 1 for MEM/EX forwarding; 2 for EX/EX forwarding of Rt.
-	output wire [1:0] ForwardBE;
-	// 1 if we need to stall for the syscall.
-	wire stallSyscall;
-	// Branch stall when a branch is taken (so the next PC is still decoding)
-	wire branchStall;
-	// Additional stall while we wait for load word's WB stage
-	wire lwStall;
-	// branchStall is high if we're branching and currently writing to a source reg
-	assign branchStall = (BranchD && RegWriteE && 
-						  (WriteRegE == RsD || WriteRegE == RtD))
-					  || (BranchD && MemtoRegM && 
-						  (WriteRegM == RsD || WriteRegM == RtD));
-
-	// lwStall is high when we're writing from memory to a reg
-	assign lwStall = ((RsD == RtE) || (RtD == RtE)) && MemtoRegE;
-
-	// Stall when either stall signal has been set 
-	assign StallF = (branchStall || lwStall );
-	assign StallD = (branchStall || lwStall );
-
-	// Flush when either stall signal has been set
-	assign FlushE = (branchStall || lwStall );
-
-	// Assign EX/EX or MEM/EX forwarding of Rs as appropriate
-	assign ForwardAE = ((RsE != 0) && (RsE == WriteRegM) && RegWriteM)  ? 2'b10 : // EX/EX
-					   (((RsE != 0) && (RsE == WriteRegW) && RegWriteW) ? 2'b01 : // MEM/EX
-					   0);
-
-	// Assign EX/EX or MEM/EX forwarding of Rt as appropriate
-	assign ForwardBE = ((RtE != 0) && (RtE == WriteRegM) && RegWriteM)  ? 2'b10 : // EX/EX
-					   (((RtE != 0) && (RtE == WriteRegW) && RegWriteW) ? 2'b01 : // MEM/EX
-					   0);
+    if (ID_EX_MemRead == 1 && (
+    (ID_EX_RegRt == IF_ID_RegRs) || (ID_EX_RegRt == IF_ID_RegRt)) begin
+        Mux_Select_Stall <= 1;
+    end
 
 endmodule

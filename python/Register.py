c5126d0697999b87cdf9223e4d36e37bcc2dbec4
KGMaxey <kgm46@txstate.edu>
2018-04-21 01:59:14 -0500

    Completed Register.py - Added correct clocking for read and write

--- python/Register.py
+++ python/Register.py
@@ -15,27 +15,34 @@ from myhdl import *
 def RegisterFile(BusA, BusB, BusW, RA, RB, RW, RegWr, clk, Rst):
 
     registers = []
+    regSig = Signal(0)
+    period = 10
 
+    #Generate Signals of intbv type for registers (Signal is used to track on gtkwave)
     for i in range(0, 32):
         registers.append(Signal(intbv(0, 0, 2**32)))
         registers[i].driven = not registers[i].driven
 
-    @always(clk.posedge)
-    def executePos():
+    #Create clocking iterator to switch every quarter full clock cycle (used to read and write on quarter of clock cycle)
+    @instance
+    def regSig_generator():
+        highTime = int(period/2)
+        lowTime = period - highTime
 
-      if RegWr:
+        while True:
+          yield delay(highTime)
+          regSig.next = 1
+          yield delay(lowTime)
+          regSig.next = 0
+
+    #Every posedge of regSig (a.k.a. every quareter clock cycle), perform either read or write (notice write will happen before read)
+    @always(regSig.posedge)
+    def writeReg():
         nonlocal registers
-        registers[RW.unsigned()].next = BusW
+        if(clk and RegWr):
+            registers[RW.unsigned()].next = BusW
+        elif(not clk):
+            BusA.next = registers[int(RA)]
+            BusB.next = registers[int(RB)]
 
-      elif Rst:
-        for i in range(0, 32):
-         registers[i] = intbv(0, 0, 2**32)
-
-    @always(clk.negedge)
-    def executeNeg():
-      nonlocal registers
-
-      BusA.next = registers[int(RA)]
-      BusB.next = registers[int(RB)]
-
-    return executePos, executeNeg
+    return regSig_generator, writeReg

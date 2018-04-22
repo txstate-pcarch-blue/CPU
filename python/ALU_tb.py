c785505f4fb6aaaf77b53bc6450644192b6c888b
mtvjr <michael.volling@g.austincc.edu>
2018-04-22 11:11:31 -0500

    Removed unneeded ALU instructions

--- python/ALU_tb.py
+++ python/ALU_tb.py
@@ -8,18 +8,17 @@ if (__name__ == "__main__"):
     reset = Signal(intbv(0, 0, 2**1))
     readA = Signal(intbv(0, 0, 2**32))
     readB = Signal(intbv(0, 0, 2**32))
-    CTRL = Signal(intbv(0, 0, 2**3))
+    CTRL = Signal(intbv(0, 0, 2**2))
     R = Signal(intbv(0, 0, 2**32))
     zero = Signal(intbv(0, 0, 2**1))
     ovf = Signal(intbv(0, 0, 2**1))
-    branch = Signal(intbv(0, 0, 2**1))
 
     clock_driver = clock_generator(clk)
 
-    ALUDriver = traceSignals(alu(clk, reset, readA, readB, CTRL, R, zero, ovf, branch))
+    ALUDriver = traceSignals(alu(clk, reset, readA, readB, CTRL, R, zero, ovf))
     readADriver = random_signal(readA, clk)
     readBDriver = random_signal(readB, clk)
     CTRLDriver = random_signal(CTRL, clk)
 
-    sim = Simulation(clock_driver, ALUDriver, readADriver, readBDriver, CTRLDriver)
+    sim = Simulation(instances())
     sim.run(1000)

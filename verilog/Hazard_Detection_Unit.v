767936b55420e8a4106cb95b837152968532b0d3
boris <borislav.sabotinov@gmail.com>
2018-04-22 20:09:31 -0500

    Boris: update to hazard detection unit.

--- verilog/Hazard_Detection_Unit.v
+++ verilog/Hazard_Detection_Unit.v
@@ -8,12 +8,15 @@ module hazard_unit(ID_EX_MemRead, ID_EX_RegRt, IF_ID_RegRs, IF_ID_RegRt);
     // Outputs: two control signals, which determine if pipeline stalls or continues
     // Mux select which forces control signals for current EX and future MEM_WB stage to 0
     // in case of stall
-    reg PCWrite, IF_ID_Write, Mux_Select_Stall;
+    output reg PCWrite, IF_ID_Write, Mux_Select_Stall;
 
-    always @ * begin
+    always @( ID_EX_MemRead, ID_EX_RegRt, IF_ID_RegRs, IF_ID_RegRt ) begin
         if (ID_EX_MemRead == 1 && ((ID_EX_RegRt == IF_ID_RegRs) || (ID_EX_RegRt == IF_ID_RegRt))) begin
-            Mux_Select_Stall <= 1;
-        end
+            Mux_Select_Stall <= 1; PCWrite <= 0; IF_ID_Write <= 0;
+        end 
+		else begin
+			Mux_Select_Stall <= 0; PCWrite <= 1; IF_ID_Write <= 1;
+		end
     end
 
 endmodule

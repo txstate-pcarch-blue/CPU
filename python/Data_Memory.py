338420b7d42f2270d06b85f9437b03d5db26b172
mtvjr <michael.volling@g.austincc.edu>
2018-04-23 09:38:44 -0500

    Updated the Instruction Memory to remove unneeded parameter.

--- python/Data_Memory.py
+++ python/Data_Memory.py
@@ -1,7 +1,7 @@
 from myhdl import *
 
 @block
-def Data_Memory(clk, address, write, read, readData, writeData, inmem):
+def Data_Memory(clk, address, write, read, readData, writeData):
     #modeled inputs and outputs to match model below
 
     #Use this link's datapath as an example (http://d2vlcm61l7u1fs.cloudfront.net/media%2F63b%2F63b8364b-1ba0-467c-87f2-60b74041b998%2FphpArhSpB.png)

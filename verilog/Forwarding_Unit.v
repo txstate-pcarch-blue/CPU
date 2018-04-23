c84ec2bcdab6a19ca705f18aac2044d13d95394f
Daniel Le <ledanieln@gmail.com>
2018-04-22 22:14:34 -0500

    Added ForwardingUnit_Tb and edited ForwardingUnit.v so it will compile

--- verilog/Forwarding_Unit.v
+++ verilog/Forwarding_Unit.v
@@ -3,31 +3,35 @@
 //data hazard - operand come from either EX_MEM or MEM_WB pipeline reg
 //MEM/WB hazard may occur between an instruction in the EX stage and the instruction from two cycles ago 
 module ForwardingUnit(
-    // Inputs: the two two RegWrite signals come from the control unit
-    ID_EX_RegRs, ID_EX_RegRt, EX_MEM_RegRd, EX_MEM_RegWrite, MEM_WB_RegRd, MEM_WB_RegWrite);
+
+    // Inputs: the two RegWrite signals come from the control unit
+    input [31:0] ID_EX_RegRs, ID_EX_RegRt, EX_MEM_RegRd, MEM_WB_RegRd,
+    input MEM_WB_RegWrite, EX_MEM_RegWrite,
 	
     // Outputs: multiplexers attached to the ALU
-    Mux_ForwardA, Mux_ForwardB
+    output reg [31:0] Mux_ForwardA, Mux_ForwardB
 );
 
-    // EX_MEM Hazard equations
-    if (EX_MEM_RegWrite == 1 && EX_MEM_RegRd == ID_EX_RegRs) begin
-        Mux_ForwardA <= 2;
-    end
-	
-    if (EX_MEM_RegWrite == 1 && EX_MEM_RegRd == ID_EX_RegRt) begin
-        Mux_ForwardB = 2;
-	end
+    always @(ID_EX_RegRs, ID_EX_RegRt, EX_MEM_RegRd, EX_MEM_RegWrite, MEM_WB_RegRd, MEM_WB_RegWrite ) begin
 
-    // MEM_WB Hazard equations
-    if(MEM_WB_RegWrite == 1 && MEM_WB_RegRd == ID_EX_RegRs &&
-    (EX_MEM_RegRd != ID_EX_RegRs || EX_MEM_RegWrite == 0)) begin
-        Mux_ForwardA <= 1;
-    end
-	
-    if (MEM_WB_RegWrite == 1 && MEM_WB_RegRd == ID_EX_RegRt &&
-    (EX_MEM_RegRd != ID_EX_RegRt || EX_MEM_RegWrite == 0)) begin
-        Mux_ForwardB <= 1;
-    end
-	
+        // EX_MEM Hazard equations
+        if (EX_MEM_RegWrite == 1 && EX_MEM_RegRd == ID_EX_RegRs) begin
+            Mux_ForwardA <= ID_EX_RegRs;
+        end
+    	
+        if (EX_MEM_RegWrite == 1 && EX_MEM_RegRd == ID_EX_RegRt) begin
+            Mux_ForwardB <= ID_EX_RegRt;
+    	end
+
+        // MEM_WB Hazard equations
+        if(MEM_WB_RegWrite == 1 && MEM_WB_RegRd == ID_EX_RegRs &&
+        (EX_MEM_RegRd != ID_EX_RegRs || EX_MEM_RegWrite == 0)) begin
+            Mux_ForwardA <= ID_EX_RegRs;
+        end
+    	
+        if (MEM_WB_RegWrite == 1 && MEM_WB_RegRd == ID_EX_RegRt &&
+        (EX_MEM_RegRd != ID_EX_RegRt || EX_MEM_RegWrite == 0)) begin
+            Mux_ForwardB <= ID_EX_RegRt;
+        end
+	end
 endmodule

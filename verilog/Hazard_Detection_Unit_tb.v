f6e663a4718bad02e6a57dd7e77f157e933c7d00
cksears <cks75@txstate.edu>
2018-04-22 21:51:52 -0500

    Completed Verilog Hazard Unit Detection Test Bench (tested in GTKWave)

--- verilog/Hazard_Detection_Unit_tb.v
+++ verilog/Hazard_Detection_Unit_tb.v
@@ -5,6 +5,10 @@ module tb();
     // Inputs
 	reg ID_EX_MemRead, ID_EX_RegRt, IF_ID_RegRs, IF_ID_RegRt;
 
+	parameter tck = 10; ///< clock tick
+	reg clk;
+	always #(tck/2) clk <= ~clk; // clocking device
+
     // Outputs: two control signals, which determine if pipeline stalls or continues
     // Mux select which forces control signals for current EX and future MEM_WB stage to 0
     // in case of stall
@@ -12,15 +16,11 @@ module tb();
 	
 	integer seed = 1;
 
-	hazard_unit dut
-	(
-		.ID_EX_MemRead(ID_EX_MemRead),
-		.ID_EX_RegRt(ID_EX_RegRt),
-		.IF_ID_RegRs(IF_ID_RegRs),
-		.IF_ID_RegRt(IF_ID_RegRt)/*, // giving error: port ``PCWrite'' is not a port of dut.
-		.PCWrite(PCWrite),
-		.IF_ID_Write(IF_ID_Write),
-		.Mux_Select_Stall(Mux_Select_Stall)*/
+	hazard_unit dut (
+		ID_EX_MemRead,
+		ID_EX_RegRt,
+		IF_ID_RegRs,
+		IF_ID_RegRt
 	);
 	  
 	integer num_iter = 0;
@@ -31,22 +31,25 @@ module tb();
 	end
 
 	initial begin
+		clk = 0;
 		ID_EX_MemRead = 0;
 		ID_EX_RegRt = 0;
 		IF_ID_RegRs = 0;
 		IF_ID_RegRt = 0;
 	end
+
+	always @(posedge clk) begin
+		num_iter++;
+	end
 	
 	always @ * begin
 
-		ID_EX_MemRead = $random(seed);
-		ID_EX_RegRt = $random(seed);
-		IF_ID_RegRs = $random(seed);
-		IF_ID_RegRt = $random(seed);
+		ID_EX_MemRead <= $random(seed);
+		ID_EX_RegRt <= $random(seed);
+		IF_ID_RegRs <= $random(seed);
+		IF_ID_RegRt <= $random(seed);
 
-		#50; // delay driven (no clock)
-
-		num_iter = num_iter + 1;
+		#5; // delay makes results more readable
 
 		if (num_iter > 5000)
 			$finish;

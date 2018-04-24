e78c2af28f7b900f7931b3f3110f548364b4975d
mtvjr <michael.volling@g.austincc.edu>
2018-04-23 19:11:16 -0500

    Updated the sign extender.

--- python/Sign_Extender.py
+++ python/Sign_Extender.py
@@ -4,12 +4,14 @@ from myhdl import *
 def Sign_Extender(clk, instruction, output):
     #Instruction should be a 16-bit intbv
     #output is accurate 32-bit intbv.
-    output = intbv(max = 2**32)
-    
-    @always(clk.posedge)
+    @always_comb
     def execute():
+        out = intbv(0, 0, 2**32)
         #Insert 16 to 32-bit conversion logic
-        for out in output[31:16]:
-            output.next = instruction[15] #make the first new bits match the first bit
+        for i in range(0, 16):
+            out[i] = instruction[i]
+        for i in range(16, 32):
+            out[i] = instruction[15] #make the first new bits match the first bit
+        output.next = out
 
     return execute
--- python/Sign_Extender_TB.py
+++ python/Sign_Extender_TB.py
@@ -9,13 +9,13 @@ from helpers.Random_Signal import random_signal
 if (__name__ == "__main__"):
     MAX_CYCLES = 1000
 
-    instruction = Signal(intbv(0, 0, 2**32))
-    output  = Signal(intbv(0, 0, 2**32))
+    instruction = Signal(intbv(0, 0, 2**16))
+    output = Signal(intbv(0, 0, 2**32))
     clk = Signal(0)
 
     pc_driver = traceSignals(Sign_Extender(clk, instruction, output))
     clock_driver = clock_generator(clk)
-    rand_driver  = random_signal(clk, instruction)
+    rand_driver = random_signal(clk, instruction)
   
     sim = Simulation(clock_driver, rand_driver, pc_driver)
     sim.run(MAX_CYCLES)
